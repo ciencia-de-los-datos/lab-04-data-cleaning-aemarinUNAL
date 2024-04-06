a3a259f8f85840006db194791f924d5098568913
aemarinUNAL <andmarinma@unal.edu.co>
2024-04-06 02:10:10 -0500

    lab-04

--- pregunta.py
+++ pregunta.py
@@ -11,10 +11,26 @@ import pandas as pd
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv(
+        "solicitudes_credito.csv", 
+        sep=";",
+        index_col=[0]   
+    )
 
-    #
-    # Inserte su código aquí
-    #
+    df.dropna(how='any',inplace=True)
 
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype('int')
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], dayfirst=True, format='mixed')
+
+    for columna in df.columns:
+        if df[columna].dtype == 'object':
+            df[columna] = df[columna].str.lower()
+
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(r'[$,]', '', regex=True).astype('float').astype('int')
+    df['línea_credito'] = df['línea_credito'].str.replace(r'[_-]', ' ', regex=True)
+    df['idea_negocio'] = df['idea_negocio'].str.replace(r'[._-]', ' ', regex=True).replace(r'\s+', ' ', regex=True).str.rstrip()
+    df['idea_negocio'] = df['idea_negocio'].str.replace(' de','').str.replace(' en','').str.replace(' y','').str.replace(' el','')
+    df['barrio'] = df['barrio'].str.replace(r'[._-]', ' ', regex=True)
+    df.drop_duplicates(inplace=True)
+    
     return df
